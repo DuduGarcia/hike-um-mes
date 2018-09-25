6c92c9d8530c0d5882095d708e32c276013f6da6
Eduardo Garcia <eduardog.silva@hotmail.com>
2018-09-25 16:33:48 -0300

    Impedir posts vazios no cliente e no servidor

--- collections/Posts.js
+++ collections/Posts.js
@@ -2,22 +2,20 @@ Posts = new Mongo.Collection("posts");
 
 Meteor.methods({
     "inserirPost": function(textoDoFormulario) {
-    	if(Meteor.userId() !== null) {
-        Posts.insert({
-            texto: textoDoFormulario,
-            idDoAutor: Meteor.userId(),
-            curtidas:[]
-        });
-   
-      }
+        if(Meteor.userId() !== null && textoDoFormulario) {
+            Posts.insert({
+                texto: textoDoFormulario,
+                idDoAutor: Meteor.userId(),
+                curtidas: []
+            });
+        }
     },
-    "curtirPost": function(idDoPost){
-    	Posts.update(idDoPost, {
+    "curtirPost": function(idDoPost) {
+        Posts.update(idDoPost, {
             $addToSet: {
                 curtidas: Meteor.userId()
             }
-       });
-
+        });
     },
     "descurtirPost": function(idDoPost) {
         Posts.update(idDoPost, {
