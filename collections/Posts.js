10f075395f4e8e020bd7dc7689364e64199c2658
Eduardo Garcia <eduardog.silva@hotmail.com>
2018-09-25 12:19:08 -0300

    Implementar curtidas.

--- collections/Posts.js
+++ collections/Posts.js
@@ -2,9 +2,28 @@ Posts = new Mongo.Collection("posts");
 
 Meteor.methods({
     "inserirPost": function(textoDoFormulario) {
+    	if(Meteor.userId() !== null) {
         Posts.insert({
             texto: textoDoFormulario,
-            idDoAutor: Meteor.userId()
+            idDoAutor: Meteor.userId(),
+            curtidas:[]
+        });
+   
+      }
+    },
+    "curtirPost": function(idDoPost){
+    	Posts.update(idDoPost, {
+            $addToSet: {
+                curtidas: Meteor.userId()
+            }
+       });
+
+    },
+    "descurtirPost": function(idDoPost) {
+        Posts.update(idDoPost, {
+           $pull: {
+                curtidas: Meteor.userId()  
+           } 
         });
     }
 });
